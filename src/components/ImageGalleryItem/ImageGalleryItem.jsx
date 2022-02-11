e9756424a6d885fc2cdbbd40e4dad9729f67bf5c
forysiak-vova <volodja.forus@gmail.com>
2022-02-11 08:50:37 +0100

    alt

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,7 +1,7 @@
 function ImageGalleryItem({ id, webformatURL, largeImageURL }) {
   return (
     <li>
-      <img src={webformatURL} width="300" />
+      <img src={webformatURL} alt="" width="300" />
     </li>
   );
 }
