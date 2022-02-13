ee9e469c28296804306eb199137670d2a1564ded
forysiak-vova <volodja.forus@gmail.com>
2022-02-13 12:28:36 +0100

    e

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ export class App extends Component {
   };
 
   clickImage = e => {
-    const srcImg = e.target.getAttribute('src');
+    const srcImg = e.target.getAttribute('srcc');
     const altImg = e.target.getAttribute('alt');
 
     this.setState({
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -12,7 +12,7 @@ function ImageGalleryItem({
         src={webformatURL}
         alt={tags}
         width="300"
-        src={largeImageURL}
+        srcc={largeImageURL}
         onClick={clickImage}
       />
     </ImageItem>
